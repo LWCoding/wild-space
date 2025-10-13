d5ee65bc58098d8e8a0f3b0100c67bf39ea18e93
LWCoding <DiamondMob01@gmail.com>
2025-10-13 16:30:24 -0700

    Set up more Yarn stuff

---
+++ Assets/Scripts/YarnDialogueManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Yarn.Unity;
+
+public class YarnDialogueManager : MonoBehaviour
+{
+    [SerializeField] private YarnProject mainYarnProject;
+    [SerializeField] private DialogueRunner dialogueRunner;
+
+    async void Start()
+    {
+        // Load the entire project (all connected .yarn files)
+        dialogueRunner.SetProject(mainYarnProject);
+
+        // Start with a specific node
+        await dialogueRunner.StartDialogue("Intro");
+    }
+}
