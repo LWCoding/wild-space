92572b7715d1fb35716e318df770add0d620434f
LWCoding <DiamondMob01@gmail.com>
2025-10-15 02:14:14 -0700

    Added new thinking sprite

--- Assets/Scripts/CharacterExpression.cs
+++ Assets/Scripts/CharacterExpression.cs
@@ -6,12 +6,8 @@ public class CharacterExpression : ScriptableObject
     [Header("Expression Info")]
     public string expressionName;
     public Sprite expressionSprite;
-    
+
     [Header("Voice Blip")]
     public AudioClip voiceBlip;
-    [Range(0.1f, 2f)]
-    public float blipInterval = 0.3f;
-    
-    [TextArea(2, 4)]
-    public string description;
+    public float blipInterval = 0.1f;
 }
