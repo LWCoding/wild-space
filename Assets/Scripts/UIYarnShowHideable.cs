a15430b2895a85398fd4b81ec049d8cffaeb430c
LWCoding <DiamondMob01@gmail.com>
2025-10-19 21:41:42 -0700

    Added more plot; things are now more connected (2/3 endings)

--- Assets/Scripts/UIYarnShowHideable.cs
+++ Assets/Scripts/UIYarnShowHideable.cs
@@ -42,6 +42,12 @@ public class UIYarnShowHideable : MonoBehaviour
         {
             text.enabled = true;
         }
+
+        // Also show any children under this GameObject
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
         Debug.Log($"Showed UI object: {gameObject.name}");
     }
 
@@ -61,6 +67,12 @@ public class UIYarnShowHideable : MonoBehaviour
         if (text != null)
         {
             text.enabled = false;
+        }
+
+        // Also hide any children under this GameObject
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
         }
         Debug.Log($"Hid UI object: {gameObject.name}");
     }
